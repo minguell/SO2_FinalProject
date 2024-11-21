5cbbb8bb1324a626e75bb7f503033c0afef0759e
KarablasteNM <mattes734@gmail.com>
2024-11-21 13:26:00 -0300

    cabeçalho

--- client.c
+++ client.c
@@ -1,3 +1,6 @@
+// Parte 1 - Sistemas Operacionais 2 (2024/2) - Weverton Cordeiro
+// Grupo: Bruno Alexandre - 00550177, Miguel Dutra - 00342573 e Nathan Mattes - 00342941
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
